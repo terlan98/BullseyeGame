ecc3d62ed81e589c052852e1982c33a62c0eeb5f
Tarlan Ismayilsoy <terlanismayilsoy@terlans-macbook.local>
2018-01-02 21:54:13 +0400

    Initial Commit

---
+++ Bullseye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Tarlan Ismayilsoy on 1/2/18.
+//  Copyright © 2018 Terlan Ismayilsoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
